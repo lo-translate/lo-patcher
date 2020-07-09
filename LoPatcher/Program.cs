db9c28fefca3b9ae4e36943c5232259bd077069c
Rashime <3317678+rashime@users.noreply.github.com>
2020-07-08 21:43:56 -0400

    Added using

--- LoPatcher/Program.cs
+++ LoPatcher/Program.cs
@@ -17,7 +17,9 @@ namespace LoPatcher
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+
+            using var form = new MainForm();
+            Application.Run(form);
         }
     }
 }
