38704161568d9d5c71f97b7f171e1295237d29ae
Rashime <3317678+rashime@users.noreply.github.com>
2020-07-08 20:19:09 -0400

    Initial form design

--- LoPatcher/MainForm.Designer.cs
+++ LoPatcher/MainForm.Designer.cs
@@ -28,13 +28,131 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dialogChooseBundle = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.labelSelectedFile = new System.Windows.Forms.Label();
+            this.buttonChooseBundle = new System.Windows.Forms.Button();
+            this.buttonPatch = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dialogChooseBundle
+            // 
+            this.dialogChooseBundle.FileName = "__data";
+            this.dialogChooseBundle.Filter = "__data files|__data*|All files|*.*";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.labelSelectedFile);
+            this.groupBox1.Controls.Add(this.buttonChooseBundle);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(379, 57);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Bundle file";
+            // 
+            // labelSelectedFile
+            // 
+            this.labelSelectedFile.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelSelectedFile.AutoEllipsis = true;
+            this.labelSelectedFile.Location = new System.Drawing.Point(93, 28);
+            this.labelSelectedFile.Name = "labelSelectedFile";
+            this.labelSelectedFile.Size = new System.Drawing.Size(275, 20);
+            this.labelSelectedFile.TabIndex = 1;
+            this.labelSelectedFile.Text = "No file selected";
+            // 
+            // buttonChooseBundle
+            // 
+            this.buttonChooseBundle.Location = new System.Drawing.Point(11, 22);
+            this.buttonChooseBundle.Name = "buttonChooseBundle";
+            this.buttonChooseBundle.Size = new System.Drawing.Size(75, 25);
+            this.buttonChooseBundle.TabIndex = 0;
+            this.buttonChooseBundle.Text = "Browse...";
+            this.buttonChooseBundle.UseVisualStyleBackColor = true;
+            this.buttonChooseBundle.Click += new System.EventHandler(this.buttonChooseBundle_Click);
+            // 
+            // buttonPatch
+            // 
+            this.buttonPatch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonPatch.Enabled = false;
+            this.buttonPatch.Location = new System.Drawing.Point(290, 150);
+            this.buttonPatch.Name = "buttonPatch";
+            this.buttonPatch.Size = new System.Drawing.Size(102, 25);
+            this.buttonPatch.TabIndex = 1;
+            this.buttonPatch.Text = "Start Patching";
+            this.buttonPatch.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Enabled = false;
+            this.groupBox2.Location = new System.Drawing.Point(13, 81);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(379, 57);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Language data";
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Location = new System.Drawing.Point(249, 20);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(119, 25);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Check for update";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 15);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Last updated: 2020-04-05";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(404, 186);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.buttonPatch);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Lo Patcher";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.OpenFileDialog dialogChooseBundle;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label labelSelectedFile;
+        private System.Windows.Forms.Button buttonChooseBundle;
+        private System.Windows.Forms.Button buttonPatch;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- LoPatcher/MainForm.cs
+++ LoPatcher/MainForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,10 +13,44 @@ namespace LoPatcher
 {
     public partial class MainForm : Form
     {
+        private string selectedFile;
+        private string defaultSelectedFileText;
+
         public MainForm()
         {
             InitializeComponent();
+
+            defaultSelectedFileText = labelSelectedFile.Text;
         }
 
+        private void Reset()
+        {
+            selectedFile = string.Empty;
+            labelSelectedFile.Text = defaultSelectedFileText;
+            buttonPatch.Enabled = false;
+        }
+
+        private void buttonChooseBundle_Click(object sender, EventArgs e)
+        {
+            if (dialogChooseBundle.ShowDialog() == DialogResult.OK)
+            {
+                selectedFile = dialogChooseBundle.FileName;
+                labelSelectedFile.Text = GetShorterDirectory(selectedFile);
+                buttonPatch.Enabled = true;
+            }
+        }
+
+        /// <summary>
+        /// Returns the specified number of parts from the specified path.
+        /// </summary>
+        /// <param name="path">The path to shorten</param>
+        /// <param name="partsToKeep">The number of parts to keep</param>
+        /// <returns></returns>
+        private static string GetShorterDirectory(string path, int partsToKeep = 2)
+        {
+            var parts = path.Split(Path.DirectorySeparatorChar);
+
+            return string.Join(Path.DirectorySeparatorChar, parts.Skip(Math.Max(0, parts.Length - partsToKeep)));
+        }
     }
 }
