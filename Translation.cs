9a38b17e1fdde7ce56adc886246b4db56b5a3957
Rashime <3317678+rashime@users.noreply.github.com>
2020-07-18 03:28:01 -0400

    Added warning disable

--- Translation.cs
+++ Translation.cs
@@ -4,11 +4,13 @@ namespace LoExtractText
 {
     internal class Translation
     {
+#pragma warning disable CS0649 // Field is never assigned to
         [FieldQuoted('"', QuoteMode.OptionalForBoth)]
         public string Korean;
 
         [FieldQuoted('"', QuoteMode.OptionalForBoth)]
         public string English;
+#pragma warning restore CS0649 // Field is never assigned to
 
         [DelimitedRecord("\t")]
         public class TsvTranslation : Translation
