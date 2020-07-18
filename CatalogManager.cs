16a9783643758b96a03d936f725215261437ccb1
Rashime <3317678+rashime@users.noreply.github.com>
2020-07-18 14:49:19 -0400

    Updated translator comment to include partial source for context

--- CatalogManager.cs
+++ CatalogManager.cs
@@ -18,7 +18,7 @@ namespace LoTextExtractor
             return catalog.Count;
         }
 
-        public void AddToCatalog(string japaneseText, string koreanText, string englishText, string sourceComment, int sourceLine)
+        public void AddToCatalog(string japaneseText, string koreanText, string englishText, string source, int sourceLine)
         {
             var key = new POKey(japaneseText);
             if (catalog.Contains(key))
@@ -26,34 +26,54 @@ namespace LoTextExtractor
                 return;
             }
 
-            var sourceReference = new POSourceReference[]
-            {
-                new POSourceReference(sourceComment, sourceLine)
-            };
-
             var newEntry = new POSingularEntry(key)
             {
                 Comments = new List<POComment>
                 {
-                    new POReferenceComment { References = sourceReference },
+                    new POReferenceComment
+                    {
+                        References = new POSourceReference[]
+                        {
+                            new POSourceReference(source, sourceLine)
+                        }
+                    }
                 },
             };
 
             if (!string.IsNullOrEmpty(englishText))
             {
-                newEntry.Translation = englishText;
+                newEntry.Translation = englishText.Trim(' ');
             }
 
+            // We render a partial source in a comment as well so it is visible in poedit and gives immediate context
+            // (the source reference added above can be viewed on right click but that is annoying).
+            var comment = source.IndexOf("LocalizationPatch") == -1
+                ? source.Replace("LastOnTable.", "")
+                        .Replace("_TableManager.", "")
+                        .Replace("_Table_", "")
+                : "";
+
+            // If we have Korean text add it to the comment as well. This gives us multiple strings to throw at a
+            // machine translator to hopefully get better context.
             if (!string.IsNullOrEmpty(koreanText) && koreanText != japaneseText)
             {
-                // The comment we save the Korean text in can't contain new lines
-                koreanText = koreanText.Replace("\r", "\\r")
-                                       .Replace("\n", "\\n")
-                                       .Replace("\t", "\\t");
+                if (comment.Length > 0)
+                {
+                    comment += " -- ";
+                }
+                
+                // The comment we save the Korean text in can't contain new lines, tabs are just for visual convenience
+                comment += "Korean Text: " + koreanText.Replace("\r", "\\r")
+                                                       .Replace("\n", "\\n")
+                                                       .Replace("\t", "\\t");
 
+            }
+
+            if (!string.IsNullOrEmpty(comment))
+            {
                 newEntry.Comments.Add(new POTranslatorComment()
                 {
-                    Text = $"Korean Text: {koreanText}"
+                    Text = comment
                 });
             }
 
